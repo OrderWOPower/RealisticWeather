fe8012d74d8d21dbb4f1d99546b19f1777e56698
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2022-12-17 11:32:33 +0800

    Add files via upload

--- GameModels/RealisticWeatherAgentStatCalculateModel.cs
+++ GameModels/RealisticWeatherAgentStatCalculateModel.cs
@@ -1,39 +1,34 @@
-﻿using SandBox.GameComponents;
+﻿using TaleWorlds.Core;
 using TaleWorlds.Engine;
 using TaleWorlds.MountAndBlade;
 
 namespace RealisticWeather.GameModels
 {
-    public class RealisticWeatherAgentStatCalculateModel
+    public class RealisticWeatherAgentStatCalculateModel : AgentStatCalculateModel
     {
-        public class RealisticWeatherCustomBattleAgentStatCalculateModel : CustomBattleAgentStatCalculateModel
+        private readonly AgentStatCalculateModel _model;
+
+        public RealisticWeatherAgentStatCalculateModel(AgentStatCalculateModel model) => _model = model;
+
+        public override void UpdateAgentStats(Agent agent, AgentDrivenProperties agentDrivenProperties)
         {
-            private AgentStatCalculateModel _model;
-
-            public RealisticWeatherCustomBattleAgentStatCalculateModel(AgentStatCalculateModel model) => _model = model;
-
-            public override void UpdateAgentStats(Agent agent, AgentDrivenProperties agentDrivenProperties)
-            {
-                Scene scene = Mission.Current.Scene;
-                base.UpdateAgentStats(agent, agentDrivenProperties);
-                _model.UpdateAgentStats(agent, agentDrivenProperties);
-                RealisticWeatherHelper.SetWeatherEffectsOnAgent(agent, agentDrivenProperties, scene.GetRainDensity(), scene.GetFog(), RealisticWeatherManager.Current.HasDust);
-            }
+            Scene scene = Mission.Current.Scene;
+            _model.UpdateAgentStats(agent, agentDrivenProperties);
+            RealisticWeatherHelper.SetWeatherEffectsOnAgent(agent, agentDrivenProperties, scene.GetRainDensity(), scene.GetFog(), RealisticWeatherManager.Current.HasDust);
         }
 
-        public class RealisticWeatherSandboxAgentStatCalculateModel : SandboxAgentStatCalculateModel
-        {
-            private AgentStatCalculateModel _model;
+        public override bool CanAgentRideMount(Agent agent, Agent targetMount) => _model.CanAgentRideMount(agent, targetMount);
 
-            public RealisticWeatherSandboxAgentStatCalculateModel(AgentStatCalculateModel model) => _model = model;
+        public override float GetDifficultyModifier() => _model.GetDifficultyModifier();
 
-            public override void UpdateAgentStats(Agent agent, AgentDrivenProperties agentDrivenProperties)
-            {
-                Scene scene = Mission.Current.Scene;
-                base.UpdateAgentStats(agent, agentDrivenProperties);
-                _model.UpdateAgentStats(agent, agentDrivenProperties);
-                RealisticWeatherHelper.SetWeatherEffectsOnAgent(agent, agentDrivenProperties, scene.GetRainDensity(), scene.GetFog(), RealisticWeatherManager.Current.HasDust);
-            }
-        }
+        public override float GetDismountResistance(Agent agent) => _model.GetDismountResistance(agent);
+
+        public override float GetKnockBackResistance(Agent agent) => _model.GetKnockBackResistance(agent);
+
+        public override float GetKnockDownResistance(Agent agent, StrikeType strikeType = StrikeType.Invalid) => _model.GetKnockDownResistance(agent, strikeType);
+
+        public override float GetWeaponDamageMultiplier(BasicCharacterObject agentCharacter, IAgentOriginBase agentOrigin, Formation agentFormation, WeaponComponentData weapon) => _model.GetWeaponDamageMultiplier(agentCharacter, agentOrigin, agentFormation, weapon);
+
+        public override void InitializeAgentStats(Agent agent, Equipment spawnEquipment, AgentDrivenProperties agentDrivenProperties, AgentBuildData agentBuildData) => _model.InitializeAgentStats(agent, spawnEquipment, agentDrivenProperties, agentBuildData);
     }
 }
--- GameModels/RealisticWeatherBattleMoraleModel.cs
+++ GameModels/RealisticWeatherBattleMoraleModel.cs
@@ -1,29 +1,33 @@
-﻿using SandBox.GameComponents;
+﻿using TaleWorlds.Core;
 using TaleWorlds.Engine;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.ComponentInterfaces;
 
 namespace RealisticWeather.GameModels
 {
-    public class RealisticWeatherBattleMoraleModel
+    public class RealisticWeatherBattleMoraleModel : BattleMoraleModel
     {
-        public class RealisticWeatherCustomBattleMoraleModel : CustomBattleMoraleModel
+        private readonly BattleMoraleModel _model;
+
+        public RealisticWeatherBattleMoraleModel(BattleMoraleModel model) => _model = model;
+
+        // Decrease morale in Custom Battle missions according to rain density, fog density and dust.
+        public override float GetEffectiveInitialMorale(Agent agent, float baseMorale)
         {
-            // Decrease morale in Custom Battle missions according to rain density, fog density and dust.
-            public override float GetEffectiveInitialMorale(Agent agent, float baseMorale)
-            {
-                Scene scene = Mission.Current.Scene;
-                return base.GetEffectiveInitialMorale(agent, baseMorale) * RealisticWeatherHelper.GetRainEffectOnMorale(scene.GetRainDensity()) * RealisticWeatherHelper.GetFogEffectOnMorale(scene.GetFog()) * RealisticWeatherHelper.GetDustEffectOnMorale(RealisticWeatherManager.Current.HasDust);
-            }
+            Scene scene = Mission.Current.Scene;
+            return _model.GetEffectiveInitialMorale(agent, baseMorale) * RealisticWeatherHelper.GetRainEffectOnMorale(scene.GetRainDensity()) * RealisticWeatherHelper.GetFogEffectOnMorale(scene.GetFog()) * RealisticWeatherHelper.GetDustEffectOnMorale(RealisticWeatherManager.Current.HasDust);
         }
 
-        public class RealisticWeatherSandboxBattleMoraleModel : SandboxBattleMoraleModel
-        {
-            // Decrease morale in Sandbox missions according to rain density, fog density and dust.
-            public override float GetEffectiveInitialMorale(Agent agent, float baseMorale)
-            {
-                Scene scene = Mission.Current.Scene;
-                return base.GetEffectiveInitialMorale(agent, baseMorale) * RealisticWeatherHelper.GetRainEffectOnMorale(scene.GetRainDensity()) * RealisticWeatherHelper.GetFogEffectOnMorale(scene.GetFog()) * RealisticWeatherHelper.GetDustEffectOnMorale(RealisticWeatherManager.Current.HasDust);
-            }
-        }
+        public override float CalculateCasualtiesFactor(BattleSideEnum battleSide) => _model.CalculateCasualtiesFactor(battleSide);
+
+        public override (float affectedSideMaxMoraleLoss, float affectorSideMaxMoraleGain) CalculateMaxMoraleChangeDueToAgentIncapacitated(Agent affectedAgent, AgentState affectedAgentState, Agent affectorAgent, in KillingBlow killingBlow) => _model.CalculateMaxMoraleChangeDueToAgentIncapacitated(affectedAgent, affectedAgentState, affectorAgent, killingBlow);
+
+        public override (float affectedSideMaxMoraleLoss, float affectorSideMaxMoraleGain) CalculateMaxMoraleChangeDueToAgentPanicked(Agent agent) => _model.CalculateMaxMoraleChangeDueToAgentPanicked(agent);
+
+        public override float CalculateMoraleChangeToCharacter(Agent agent, float maxMoraleChange) => _model.CalculateMoraleChangeToCharacter(agent, maxMoraleChange);
+
+        public override bool CanPanicDueToMorale(Agent agent) => _model.CanPanicDueToMorale(agent);
+
+        public override float GetAverageMorale(Formation formation) => _model.GetAverageMorale(formation);
     }
 }
